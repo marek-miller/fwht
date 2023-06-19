1fcef27af7ccddf0cdb89cd6d1eeec515dd9efa5
Marek Miller <marek.miller@protonmail.com>
2023-06-19 23:10:51 +0200

    Reformat

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,12 @@
 //! Wikipedia: https://en.wikipedia.org/wiki/Fast_Walsh%E2%80%93Hadamard_transform
 //! See also: https://doi.org/10.1109/TC.1976.1674569
 
-use std::ops::{Add, Sub};
+use std::ops::{
+    Add,
+    AddAssign,
+    Sub,
+    SubAssign,
+};
 
 /// In-place fast Walsh-Hadamard transform of slice `data`.
 pub fn fwht<T>(data: &mut [T])
@@ -25,30 +30,51 @@ where
     }
 }
 
-pub fn binary_dot_product(i: usize, j: usize) -> usize {
+pub fn binary_dot_product(
+    i: usize,
+    j: usize,
+) -> usize {
     (0..usize::BITS).fold(0, |acc, k| (i & j) >> k & 1 ^ acc)
 }
 
-pub fn naive_wht<T>(data: &[T], output: &mut [T])
-where
-    T: Add<T, Output = T> + Sub<T, Output = T> + Copy,
+/// Naive implementation of Walsh-Hadamard transform of slice `data`.
+///
+/// The result is written to `data`.  Uses `scratch` as a working space.
+/// Assumes `scratch.len() >= data.len()`.
+pub fn naive_wht<T>(
+    data: &mut [T],
+    scratch: &mut [T],
+) where
+    T: AddAssign + SubAssign + Copy,
 {
     let l = data.len();
-    assert_eq!(output.len(), l);
+    assert!(scratch.len() >= l);
     for i in 0..l {
         for j in 0..l {
             if binary_dot_product(i, j) == 0 {
-                output[i] = output[i] + data[j]
+                scratch[i] += data[j]
             } else {
-                output[i] = output[i] - data[j]
+                scratch[i] -= data[j]
             }
         }
+    }
+    for i in 0..l {
+        data[i] = scratch[i];
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn naive_wht_01() {
+        let data = &mut vec![1, 0, 1, 0, 0, 1, 1, 0];
+        let expected = vec![4, 2, 0, -2, 0, 2, 0, 2];
+
+        naive_wht(data, &mut vec![0; 8]);
+        assert_eq!(*data, expected);
+    }
 
     #[test]
     fn it_works() {
@@ -84,29 +110,19 @@ mod tests {
 
     #[test]
     fn binary_dot_product_02() {
-        assert_eq!(binary_dot_product(0b1011, 0b1100), 1);
-        assert_eq!(binary_dot_product(0b1011, 0b1010), 0);
-
         assert_eq!(binary_dot_product(0b1, 0b1), 1);
         assert_eq!(binary_dot_product(0b11, 0b11), 0);
         assert_eq!(binary_dot_product(0b111, 0b111), 1);
         assert_eq!(binary_dot_product(0b1111, 0b1111), 0);
         assert_eq!(binary_dot_product(0b11111, 0b11111), 1);
 
+        assert_eq!(binary_dot_product(0b1011, 0b1100), 1);
+        assert_eq!(binary_dot_product(0b1011, 0b1010), 0);
+
         assert_eq!(binary_dot_product(0b101001, 0b101001), 1);
         assert_eq!(binary_dot_product(0b10100001, 0b10100001), 1);
         assert_eq!(binary_dot_product(0b0010100001, 0b0010100001), 1);
 
         assert_eq!(binary_dot_product(0b0000100001, 0b0010100001), 0);
-    }
-
-    #[test]
-    fn naive_wht_01() {
-        let data = &mut vec![1, 0, 1, 0, 0, 1, 1, 0];
-        let expected = vec![4, 2, 0, -2, 0, 2, 0, 2];
-
-        let mut output = vec![0; 8];
-        naive_wht(data, &mut output);
-        assert_eq!(output, expected);
     }
 }
